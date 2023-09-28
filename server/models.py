357121eeaee050b83a84cdf65b2e5a172f06e7a0
Glory Gweth <glorygweth6@gmail.com>
2023-09-28 12:51:01 +0300

    complete

--- server/models.py
+++ server/models.py
@@ -12,6 +12,18 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('name')
+    def validate_name(self, key, authors):
+        if  None  in authors:
+            raise  ValueError("Author should have a name")
+        return authors
+    @validates('phone_number')
+    def validate_number(self,key,authors):
+        if len(key) !=10 in authors:
+            raise ValueError("Phone number should have 10 digits")
+        return authors
+    
+
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
 
@@ -27,6 +39,40 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Post must have a title")
+        return title
+
+    @validates('content')
+    def validate_content(self, key, content):
+        # Ensure post content is at least 250 characters long
+        if content and len(content) < 250:
+            raise ValueError("Post content must be at least 250 characters long")
+        return content
+
+
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if summary and len(summary) >= 250:
+            raise ValueError("Post summary cannot exceed 250 characters")
+        return summary
+
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ["Fiction", "Non-Fiction"]:
+            raise ValueError("Post category must be 'Fiction' or 'Non-Fiction'")
+        return category
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if not title:
+            raise ValueError("Post title cannot be empty")
+
+        clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+        if not any(phrase in title for phrase in clickbait):
+            raise ValueError("Post title should contain at least one clickbait phrase: 'Won't Believe', 'Secret', 'Top [number]', 'Guess'")
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
